9aba79f6a17fdd160e5c1c8459cff50c8be59b95
Philipp Engelhard <philippengelhard@gmail.com>
2018-03-06 17:58:57 +0100

    added Google Fit API helper

---
+++ app/src/main/java/net/quatur/googlefit/GoogleFitApiHelper.java
@@ -0,0 +1,121 @@
+package net.quatur.googlefit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.result.SessionStopResult;
+
+
+/**
+ * Created by philipp on 05.03.18.
+ */
+
+public class GoogleFitApiHelper {
+
+    public static final String TAG = GoogleFitApiHelper.class.getSimpleName();
+    private static GoogleFitApiHelper INSTANCE = null;
+    private GoogleApiClient apiClient;
+
+    private GoogleFitApiHelper() {
+    }
+
+    public static synchronized GoogleFitApiHelper getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new GoogleFitApiHelper();
+        }
+        return INSTANCE;
+    }
+
+    public void connect() {
+
+        Log.i(TAG, "connect");
+        apiClient.connect();
+    }
+
+    public void buildFitnessClient(Context context, GoogleApiClient.ConnectionCallbacks connectionCallbacks, GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
+        Log.i(TAG, "buildFitnessClient");
+        apiClient = new GoogleApiClient.Builder(context)
+                .addApi(Fitness.CONFIG_API)
+                .addApi(Fitness.RECORDING_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addConnectionCallbacks(connectionCallbacks)
+                .addOnConnectionFailedListener(onConnectionFailedListener)
+                .build();
+    }
+
+    public boolean hasClient() {
+        return (apiClient != null);
+    }
+
+    public boolean isConnected() {
+        return apiClient.isConnected();
+    }
+
+    public void unregisterListeners(GoogleApiClient.ConnectionCallbacks connectionCallbacks, GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
+        if (connectionCallbacks != null) {
+            Log.i(TAG, "unregisterConnectionCallbacks");
+            apiClient.unregisterConnectionCallbacks(connectionCallbacks);
+        }
+
+        if (onConnectionFailedListener != null) {
+            Log.i(TAG, "unregisterConnectionFailedListener");
+            apiClient.unregisterConnectionFailedListener(onConnectionFailedListener);
+        }
+    }
+
+    public void disconnect() {
+        Log.i(TAG, "disconnect");
+        apiClient.disconnect();
+    }
+
+    public boolean isConnecting() {
+        return apiClient.isConnecting();
+    }
+
+    public void revokeFitPermissions(final Activity activity, final RevokeGoogleFitPermissionsListener revokePermissionsListener) {
+        if (apiClient != null && apiClient.isConnected()) {
+            Log.i(TAG, "disableFit");
+            PendingResult<Status> pendingResult = Fitness.ConfigApi.disableFit(apiClient);
+            pendingResult.setResultCallback(new ResultCallback<Status>() {
+                @Override
+                public void onResult(Status status) {
+                    Log.i(TAG, "Disconnect fit " + status.toString() + ", code " + status.getStatus().getStatusCode());
+                    if (status.isSuccess()) {
+                        Toast.makeText(activity, "googlefit disconnect success", Toast.LENGTH_LONG).show();
+                        revokePermissionsListener.onRevokedFitPermissions();
+                    } else {
+                        Toast.makeText(activity, "googlefit disconnect failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(activity, "googlefit disconnect failed notconnected", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public PendingResult<SessionStopResult> stopSession(String oldSessionId) {
+        Log.i(TAG, "stopSession: " + oldSessionId);
+        return Fitness.SessionsApi.stopSession(apiClient, oldSessionId);
+    }
+
+    public PendingResult<Status> subscribe(final DataType dataType) {
+        Log.i(TAG, "subscribe: " + dataType.getName());
+        return Fitness.RecordingApi.subscribe(apiClient, dataType);
+    }
+
+    public interface RevokeGoogleFitPermissionsListener {
+
+        void onRevokedFitPermissions();
+    }
+}
--- app/src/main/java/net/quatur/googlefit/MainActivity.java
+++ app/src/main/java/net/quatur/googlefit/MainActivity.java
@@ -1,13 +1,150 @@
 package net.quatur.googlefit;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataType;
+
+public class MainActivity extends AppCompatActivity implements GoogleFitApiHelper.RevokeGoogleFitPermissionsListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
+    private boolean authInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button requestStepsButton = (Button)findViewById(R.id.request_steps_button);
+        requestStepsButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                GoogleFitApiHelper.getInstance().buildFitnessClient(MainActivity.this, connectionCallbacks, onConnectionFailedListener);
+                GoogleFitApiHelper.getInstance().connect();
+            }
+        });
+
+        Button logoutButton = (Button)findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                GoogleFitApiHelper.getInstance().revokeFitPermissions(MainActivity.this, MainActivity.this);
+                GoogleFitApiHelper.getInstance().disconnect();
+            }
+        });
+
+        TextView outputTextview = (TextView)findViewById(R.id.output_textview);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
+
+        if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
+            authInProgress = false;
+            if (resultCode == RESULT_OK) {
+                if (!GoogleFitApiHelper.getInstance().isConnecting() && !GoogleFitApiHelper.getInstance().isConnected()) {
+                    GoogleFitApiHelper.getInstance().connect();
+                }
+            } else {
+                // The user cancelled auth. Allow the subclass to handle updating the UI, etc.
+                handleAuthCancelled();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!GoogleFitApiHelper.getInstance().hasClient()) {
+            GoogleFitApiHelper.getInstance().buildFitnessClient(this, connectionCallbacks, onConnectionFailedListener);
+        }
+    }
+
+    private GoogleApiClient.ConnectionCallbacks connectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+
+        @Override
+        public void onConnected(Bundle bundle) {
+            Log.i(TAG, "Google Fit connected");
+
+            subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            // If your connection to the sensor gets lost at some point,
+            // you'll be able to determine the reason and react to it here.
+            if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+            } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+            }
+        }
+    };
+
+    private GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            Log.i(TAG, "Connection failed. Cause: " + result.toString());
+            if (!result.hasResolution()) {
+                GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), MainActivity.this, 0).show();
+                return;
+            }
+            if (!authInProgress) {
+                try {
+                    Log.i(TAG, "Attempting to resolve failed connection");
+                    authInProgress = true;
+                    result.startResolutionForResult(MainActivity.this, REQUEST_OAUTH_REQUEST_CODE);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(TAG, "Exception while starting resolution activity", e);
+                }
+            }
+        }
+    };
+
+    private void subscribe(final DataType dataType) {
+        PendingResult<Status> res = GoogleFitApiHelper.getInstance().subscribe(dataType);
+        res.setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                String msg;
+                if (status.isSuccess()) {
+                    if (status.getStatusCode() == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                        msg = "Already sub to " + dataType.getName();
+                    } else {
+                        msg = "Successfully subscribed! " + dataType.getName();
+                    }
+                } else {
+                    msg = "There was a problem subscribing." + dataType.getName();
+                }
+                Log.i(TAG, msg);
+            }
+        });
+    }
+
+    @Override
+    public void onRevokedFitPermissions() {
+        Log.i(TAG, "revoked fit permissions");
+//        GoogleFitApiHelper.getInstance().disconnect();
+    }
+
+    void handleAuthCancelled() {
+        Log.i(TAG, "Auth cancelled");
     }
 }
